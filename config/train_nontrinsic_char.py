2146aa63c5a60d54f11c889d7a901980e739679a
Andrew <gus123tractor@gmail.com>
2026-04-03 23:26:47 +0400

    tweak parameters

--- config/train_nontrinsic_char.py
+++ config/train_nontrinsic_char.py
@@ -3,8 +3,8 @@
 
 out_dir = 'out-nontrinsic-char'
 eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
-log_interval = 10 # don't print too too often
+eval_iters = 20
+log_interval = 10
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
@@ -15,18 +15,18 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'nontrinsic_char'
 gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+batch_size = 12
+block_size = 64 # context of up to X previous characters
 
 # baby GPT model :)
 n_layer = 6
-n_head = 6
-n_embd = 384
+n_head = 4
+n_embd = 128
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 20000
-lr_decay_iters = 5000 # make equal to max_iters usually
+lr_decay_iters = max_iters # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
